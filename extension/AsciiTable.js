6bf58d15c304ef0bb3f57890e7bc70fe9ec3cacf
Bajdzis <rafal612b@wp.pl>
2016-08-21 13:30:21 +0200

    refactoring

--- extension/AsciiTable.js
+++ extension/AsciiTable.js
@@ -1,52 +1,58 @@
 module.exports = function AsciiTable(json)
 {
+    var _this = this;
+    var MAX_CHARACTERS_IN_LINE = 230;
+    this.keys = Object.keys(json[0]);
+    this.width = {};
 
-    this.table = function(json){
-        var keys = Object.keys(json[0]);
-        var width = {};
-        var buffer = '';
+    this.table = function(){
 
-        for (var key in keys) {
-            width[keys[key]] = String(keys[key]).length;
+        for (var key in _this.keys) {
+            _this.width[_this.keys[key]] = String(_this.keys[key]).length;
         }
 
         for (var row in json) {
             for (var data in json[row]) {
-                width[data] = Math.max(width[data], String(json[row][data]).length);
+                _this.width[data] = Math.max(_this.width[data], String(json[row][data]).length);
             }
         }
 
+        return _this.draw();
+    }
+
+    this.draw = function () {
+        var buffer = '';
         // draw 
-        buffer += this.line(width);
+        buffer += _this.line();
         buffer += "\n | "
-        for (var key in keys) {
-            buffer += keys[key] + ( ' '.repeat(width[keys[key]] - String(keys[key]).length) ) + " | ";
+        for (var key in _this.keys) {
+            buffer += _this.keys[key] + ( ' '.repeat(_this.width[_this.keys[key]] - String(_this.keys[key]).length) ) + " | ";
         }
             
-        buffer += this.line(width); 
+        buffer += _this.line(); 
         
         for (var row in json) {
             
             buffer += "\n | ";
             for (var data in json[row]) {
 
-                buffer += String(json[row][data]) + ( ' '.repeat(width[data] - String(json[row][data]).length) ) + " | ";
+                buffer += String(json[row][data]) + ( ' '.repeat(_this.width[data] - String(json[row][data]).length) ) + " | ";
 
             }
         }
         
-        buffer += this.line(width); 
+        buffer += this.line(); 
         return buffer;
     }
-    
-    this.line = function(widthArray){
+
+    this.line = function(){
         var line = "\n +";
-        for (var size in widthArray) {
-            line +=  ( '-'.repeat(widthArray[size]+2) ) + "+";
+        for (var size in this.width) {
+            line +=  ( '-'.repeat(this.width[size]+2) ) + "+";
         }
 
         return line;
     }
 
-    return this.table(json);
+    return this.table();
 }
