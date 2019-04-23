7eb7d43b4a1f4fe66c2801a3b39952301a30af97
EPMatt <acc.epmatt@gmail.com>
2019-04-23 15:49:27 +0200

    fix build errors

--- src/extension/engine/AbstractServer.ts
+++ src/extension/engine/AbstractServer.ts
@@ -144,11 +144,11 @@ export class AbstractServer
      * @return {string} - the SQL string without comments
      */
     removeComments(sql: string) {
-        const quotes=/^((?:[^"`']*?(?:(?:"(?:[^"]|\\\")*?(?<!\\)")|(?:'(?:[^']|\\\')*?(?<!\\)')|(?:`(?:[^`]|\\\`)*?(?<!\\)`)))*?[^"`']*?)/;
-        const cStyleComments=new RegExp(quotes.source+`/\\*.*?\\*/`);
-        const doubleDashComments=new RegExp(quotes.source+`--.*(\r\n|\n|\r)?`);
-        while(sql.match(cStyleComments)) sql=sql.replace(cStyleComments,`$1`);
-        while(sql.match(doubleDashComments)) sql=sql.replace(doubleDashComments,`$1$2`);
+        const quotes=/^((?:[^"`']*?(?:(?:"(?:[^"]|\\")*?(?<!\\)")|(?:'(?:[^']|\\')*?(?<!\\)')|(?:`(?:[^`]|\\`)*?(?<!\\)`)))*?[^"`']*?)/;
+        const cStyleComments=new RegExp(quotes.source+'/\\*.*?\\*/');
+        const doubleDashComments=new RegExp(quotes.source+'--.*(\r\n|\n|\r)?');
+        while(sql.match(cStyleComments)) sql=sql.replace(cStyleComments,'$1');
+        while(sql.match(doubleDashComments)) sql=sql.replace(doubleDashComments,'$1$2');
         return sql;
     }
 
--- src/extension/engine/mysql-pass.ts
+++ src/extension/engine/mysql-pass.ts
@@ -120,13 +120,13 @@ export class MySQLType extends AbstractServer
      * @return {string} - the SQL string without comments
      */
     removeComments(sql: string) {
-        const quotes=/^((?:[^"`']*?(?:(?:"(?:[^"]|\\\")*?(?<!\\)")|(?:'(?:[^']|\\\')*?(?<!\\)')|(?:`(?:[^`]|\\\`)*?(?<!\\)`)))*?[^"`']*?)/;
-        const cStyleComments=new RegExp(quotes.source+`/\\*.*?\\*/`);
-        const doubleDashComments=new RegExp(quotes.source+`--[ \t]+.*(\r\n|\n|\r)?`);
-        const hashComments=new RegExp(quotes.source+`#.*(\r\n|\n|\r)?`);
-        while(sql.match(cStyleComments)) sql=sql.replace(cStyleComments,`$1`);
-        while(sql.match(doubleDashComments)) sql=sql.replace(doubleDashComments,`$1$2`);
-        while(sql.match(hashComments)) sql=sql.replace(hashComments,`$1$2`);
+        const quotes=/^((?:[^"`']*?(?:(?:"(?:[^"]|\\")*?(?<!\\)")|(?:'(?:[^']|\\')*?(?<!\\)')|(?:`(?:[^`]|\\`)*?(?<!\\)`)))*?[^"`']*?)/;
+        const cStyleComments=new RegExp(quotes.source+'/\\*.*?\\*/');
+        const doubleDashComments=new RegExp(quotes.source+'--[ \t]+.*(\r\n|\n|\r)?');
+        const hashComments=new RegExp(quotes.source+'#.*(\r\n|\n|\r)?');
+        while(sql.match(cStyleComments)) sql=sql.replace(cStyleComments,'$1');
+        while(sql.match(doubleDashComments)) sql=sql.replace(doubleDashComments,'$1$2');
+        while(sql.match(hashComments)) sql=sql.replace(hashComments,'$1$2');
         return sql;
     }
 
